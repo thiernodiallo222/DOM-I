8878a9d50dbadd866a3d8dc2cb55ebfe559f5f6e
thiernodiallo222 <thiernodiallo864@gmail.com>
2019-12-05 16:25:14 -0500

    completed project

--- js/index.js
+++ js/index.js
@@ -48,7 +48,24 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 let  conatainer = document.getElementsByClassName('.container');
 
 
-let navigation = document.querySelector('.container header nav');
+let navItemOne = document.querySelector('.container header nav >:nth-child(1)');
+navItemOne.textContent = siteContent["nav"]["nav-item-1"];
+let navItemTwo = document.querySelector('.container header nav >:nth-child(2)');
+navItemTwo.textContent = siteContent["nav"]["nav-item-2"];
+let navItemTree = document.querySelector('.container header nav >:nth-child(3)');
+navItemTree.textContent = siteContent["nav"]["nav-item-3"];
+let navItemFour = document.querySelector('.container header nav >:nth-child(4)');
+navItemFour.textContent = siteContent["nav"]["nav-item-4"];
+let navItemFive = document.querySelector('.container header nav >:nth-child(5)');
+navItemFive.textContent = siteContent["nav"]["nav-item-5"];
+let navItemSix = document.querySelector('.container header nav >:nth-child(6)');
+navItemSix.textContent = siteContent["nav"]["nav-item-6"];
+
+
+
+
+
+
 
 let anchor = document.querySelectorAll('a');
 
@@ -111,12 +128,36 @@ aboutH4.innerHTML = siteContent["main-content"]["about-h4"];
 let aboutContent = document.querySelector('.main-content .top-content >:last-child >:last-child');
 aboutContent.innerHTML = siteContent["main-content"]["about-content"];
 
+
+let botConServicesH4 = document.querySelector('.container .bottom-content >:nth-child(1) >:first-child');
+botConServicesH4.textContent = siteContent["main-content"]["services-h4"];
+
+let botConServicesContent = document.querySelector('.container .bottom-content >:nth-child(1) >:last-child');
+botConServicesContent.textContent = siteContent["main-content"]["services-content"];
+
+let botConProductH4 = document.querySelector('.container .bottom-content >:nth-child(2) >:first-child');
+botConProductH4.textContent = siteContent["main-content"]["product-h4"];
+
+
+let botConProductContent = document.querySelector('.container .bottom-content >:nth-child(2) >:last-child');
+botConProductContent.textContent = siteContent["main-content"]["product-content"];
+
+let botConVisionH4 = document.querySelector('.container .bottom-content >:nth-child(3) >:first-child');
+botConVisionH4.textContent = siteContent["main-content"]["vision-h4"];
+
+
+let botConVisionContent = document.querySelector('.container .bottom-content >:nth-child(3) >:last-child');
+botConVisionContent.textContent = siteContent["main-content"]["vision-content"];
+
+
 // main-content": {
 //     "features-h4":"Features",
 //     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 //     "about-h4":"About",
 //     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 //     "middle-img-src": "img/mid-page-accent.jpg",
+
+
 //     "services-h4":"Services",
 //     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
 //     "product-h4":"Product",
